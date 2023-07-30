96ad0e917b8f5a8717f39de686886de9e93325e0
phpleo <phpleo@gmail.com>
2023-07-30 15:19:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,3 @@
 // create web server
 // start web server
 // open in browser
-
-// 1. load http module
-var http = require('http');
-
-// 2. create web server
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey ninjas');
-});
