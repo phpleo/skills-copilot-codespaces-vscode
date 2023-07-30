1d824aa2f84cd6c32b6b32daac6ec1cc9112492b
phpleo <phpleo@gmail.com>
2023-07-30 15:13:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,22 @@
 // create web server
-// 1. load express module
-const express = require('express')
-// 2. create express object
-const app = express()
-// 3. create web server
-app.listen(3000, function () {
-  console.log('server running at http://
+// 2019-07-30    PV
+
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+
+// get all comments
+router.get('/', async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.json(comments);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
+
+// get one comment
+router.get('/:id', getComment, (req, res) => {
+    res.json(res.comment);
+})
