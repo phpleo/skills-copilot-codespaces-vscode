de29608c1fee179ccb381dcca06e72609e1127ab
phpleo <phpleo@gmail.com>
2023-07-30 15:32:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,46 @@
-var member = document.getElementById("member");
-var memberValue = member.options[member.selectedIndex].value;
-var memberText = member.options[member.selectedIndex].text;
-if (memberValue == "yes") {
-    document.getElementById("memberSkills").style.display = "block";
-} else {
-    document.getElementById("memberSkills").style.display = "none";
-}
+// Create web server
+// Run with: node comments.js
+// Then visit: http://localhost:3000
+var http = require('http');
+var url = require('url');
+var items = [];
+
+var server = http.createServer(function(req, res) {
+  switch(req.method) {
+    case 'POST':
+      var item = '';
+      req.setEncoding('utf8');
+      req.on('data', function(chunk) {
+        item += chunk;
+      });
+      req.on('end', function() {
+        items.push(item);
+        res.end('OK\n');
+      });
+      break;
+    case 'GET':
+      var body = items.map(function(item, i) {
+        return i + ') ' + item;
+      }).join('\n');
+      res.setHeader('Content-Length', Buffer.byteLength(body));
+      res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+      res.end(body);
+      break;
+    case 'DELETE':
+      var path = url.parse(req.url).pathname;
+      var i = parseInt(path.slice(1), 10);
+      if (isNaN(i)) {
+        res.statusCode = 400;
+        res.end('Invalid item id');
+      } else if (!items[i]) {
+        res.statusCode = 404;
+        res.end('Item not found');
+      } else {
+        items.splice(i, 1);
+        res.end('OK\n');
+      }
+      break;
+  }
+});
+
+server.listen(3000);
