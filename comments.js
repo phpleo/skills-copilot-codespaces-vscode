d38f488a9ce0db64cc0e5a37db92214aa649006b
phpleo <phpleo@gmail.com>
2023-07-30 15:25:15 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,8 @@
-// create web server
-// start web server
-// open in browser
+var member = document.getElementById("member");
+var memberValue = member.options[member.selectedIndex].value;
+var memberText = member.options[member.selectedIndex].text;
+if (memberValue == "yes") {
+    document.getElementById("memberSkills").style.display = "block";
+} else {
+    document.getElementById("memberSkills").style.display = "none";
+}
